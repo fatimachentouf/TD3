4e0cc95565ece9b5468ac4930252e27681604d67
fatima zohra chentouf <fatimazohra.chentouf1@etu.uae.ac.ma>
2023-11-04 20:11:22 +0100

    add ex2

---
+++ ex9.cpp
@@ -0,0 +1,6 @@
+//a. Les instructions données provoqueront probablement une erreur de compilation en raison d'une 
+// incompatibilité de type entre les types des valeurs fournies (int) et le type attendu (char).
+
+//b. Pour que les instructions fonctionnent correctement, modifiez-les comme suit : 
+point<char> p('60', '65'); 
+p.affiche();.
